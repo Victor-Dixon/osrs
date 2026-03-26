830b8bae3b0e109e78d98b8d8fd2f7f99763fc91
DaDudeKC <freerideinvestor@gmail.com>
2026-03-26 11:38:24 -0500

    Make ActionQueue tests deterministic without sleep

--- src/test/java/com/example/rsautomation/engine/ActionQueueTest.java
+++ src/test/java/com/example/rsautomation/engine/ActionQueueTest.java
@@ -20,12 +20,20 @@ class ActionQueueTest {
     void consumeClearsPendingFingerprint() throws InterruptedException {
         ActionQueue queue = new ActionQueue();
         Action first = new MineAction("first", 100, 200, null, 1);
+
+        assertTrue(queue.submit(first, true));
+        assertTrue(queue.hasPending(first.getFingerprint()));
+        assertNotNull(queue.take());
+        assertFalse(queue.hasPending(first.getFingerprint()));
+    }
+
+    @Test
+    void cooldownRejectsImmediateResubmit() {
+        ActionQueue queue = new ActionQueue();
+        Action first = new MineAction("first", 100, 200, null, 1);
         Action second = new MineAction("second", 100, 200, null, 1);
 
         assertTrue(queue.submit(first, true));
-        assertNotNull(queue.take());
-        Thread.sleep(1100);
-
-        assertTrue(queue.submit(second, true));
+        assertFalse(queue.submit(second, true));
     }
 }
